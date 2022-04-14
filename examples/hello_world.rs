1345a23a2f807dccb5eee36a00f70d5704b4c480
Pantonshire <tom@tomandtally.co.uk>
2022-04-14 14:41:46 +0100

    Update example file

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,4 +1,5 @@
-use chrono::prelude::*;
+use std::num::NonZeroU8;
+
 use tasque::*;
 
 #[derive(Copy, Clone, PartialEq, Eq)]
@@ -8,9 +9,18 @@ enum TaskId {
 }
 
 fn main() {
+    let nonzero_5 = NonZeroU8::new(5).unwrap();
+
+    let task1_schedule = Schedule::new_every_minute()
+        .at_every_nth_second(nonzero_5);
+
+    let task2_schedule = Schedule::new_every_minute()
+        .at_every_nth_second_between(1..=59, nonzero_5)
+        .unwrap();
+
     let mut scheduler = Scheduler::<TaskId, _>::new_utc();
-    scheduler.insert(Task::builder(TaskId::Task1).at(Schedule::new_every_minute().at_second(15)).build());
-    scheduler.insert(Task::builder(TaskId::Task2).at(Schedule::new_every_minute().at_second(45)).build());
+    scheduler.insert(Task::builder(TaskId::Task1).at(task1_schedule).build());
+    scheduler.insert(Task::builder(TaskId::Task2).at(task2_schedule).build());
 
     for id in scheduler.take(4) {
         run_task(id);
@@ -18,9 +28,8 @@ fn main() {
 }
 
 fn run_task(id: TaskId) {
-    print!("{:?} ", Utc::now());
     match id {
-        TaskId::Task1 => println!("Hello, world! The current second is now 15"),
-        TaskId::Task2 => println!("Hello, world! The current second is now 45"),
+        TaskId::Task1 => println!("Hello,"),
+        TaskId::Task2 => println!("world!"),
     }
 }
